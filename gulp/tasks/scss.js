89e4a00da2abbfbcbea160f2125f69112330b65d
unknown <excitenew@gmail.com>
2022-12-30 02:22:56 +0200

    Add gh-pages npm plugin

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -10,47 +10,44 @@ import webpcss from 'gulp-webpcss'; // webp images
 const sass = gulpSass(dartSass);
 
 export const scss = () => {
-	return app.gulp.src(app.path.src.scss, { sourcemaps: app.isDev })
-		.pipe(sass({
-			outputStyle: 'expanded'
-		}))
-		.pipe(app.plugins.replace(/@img\//g, '../img/'))
-		.pipe(
-			app.plugins.if(
-				app.isBuild,
-				groupCssMediaQueries()
-			)
-		)
-		.pipe(
-			app.plugins.if(
-				app.isBuild,
-				webpcss({
-					webpClass: '.webp',
-					noWebpClass: '.no-webp',
+	return (
+		app.gulp
+			.src(app.path.src.scss, { sourcemaps: app.isDev })
+			.pipe(
+				sass({
+					outputStyle: 'expanded',
 				})
 			)
-		)
-		.pipe(
-			app.plugins.if(
-				app.isBuild,
-				autoprefixer({
-					grid: true,
-					overrideBrowserslist: ['last 3 versions'],
-					cascade: true,
+			.pipe(app.plugins.replace(/@img\//g, '../img/'))
+			.pipe(app.plugins.if(app.isBuild, groupCssMediaQueries()))
+			.pipe(
+				app.plugins.if(
+					app.isBuild,
+					webpcss({
+						webpClass: '.webp',
+						noWebpClass: '.no-webp',
+					})
+				)
+			)
+			.pipe(
+				app.plugins.if(
+					app.isBuild,
+					autoprefixer({
+						grid: true,
+						overrideBrowserslist: ['last 3 versions'],
+						cascade: true,
+					})
+				)
+			)
+			// creating non minified css file if need
+			.pipe(app.gulp.dest(app.path.build.css))
+			.pipe(app.plugins.if(app.isBuild, cleanCss()))
+			.pipe(
+				rename({
+					extname: '.min.css',
 				})
 			)
-		)
-		// creating non minified css file if need
-		.pipe(app.gulp.dest(app.path.build.css))
-		.pipe(
-			app.plugins.if(
-				app.isBuild,
-				cleanCss()
-			)
-		)
-		.pipe(rename({
-			extname: '.min.css'
-		}))
-		.pipe(app.gulp.dest(app.path.build.css))
-		.pipe(app.plugins.browsersync.stream());
-}
+			.pipe(app.gulp.dest(app.path.build.css))
+			.pipe(app.plugins.browsersync.stream())
+	);
+};
